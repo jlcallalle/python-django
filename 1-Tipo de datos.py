ede1caf83ff9deeb25d5f0760074969574197b7c
Jorge <jlcallalle@gmail.com>
2019-08-25 21:25:55 -0500

    learn python

---
+++ 1-Tipo de datos.py
@@ -0,0 +1,26 @@
+""" Tipos de Datos
+
+Enteros (int): en este grupo están todos los números, enteros y long:
+ejemplo: 1, 2.3, 2121, 2192, -123
+
+Booleanos (bool): Son los valores falso o verdadero, compatibles con todas las operaciones booleanas ( and, not, or ):
+ejemplo: True, False
+
+Cadenas (str): Son una cadena de texto :
+ejemplos: “Hola”, “¿Cómo estas?”
+
+Listas: Son un grupo o array de datos, puede contener cualquiera de los datos anteriores:
+ejemplos: [1,2,3, ”hola” , [1,2,3] ], [1,“Hola”,True ]
+
+Diccionarios: Son un grupo de datos que se acceden a partir de una clave:
+ejemplo: {“clave”:”valor”}, {“nombre”:”Fernando”}
+
+Tuplas: también son un grupo de datos igual que una lista con la diferencia que una tupla después de creada no se puede modificar.
+ejemplos: (1,2,3, ”hola” , (1,2,3) ), (1,“Hola”,True) (Pero jamás podremos cambiar los elementos dentro de esa Tupla)
+
+
+
+
+
+
+"""
